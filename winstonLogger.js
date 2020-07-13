6833d714ef8cacf1ce4752afe7b6734a749ec3c4
Raunak Mokhasi <44176068+raunakmokhasi@users.noreply.github.com>
2020-07-13 16:52:06 +0400

    Add files via upload

--- winstonLogger.js
+++ winstonLogger.js
@@ -29,3 +29,42 @@ const logger = winston.createLogger({
 
 
 module.exports = logger;
+
+
+/* For Daily Logging -
+// DO - npm install winston-daily-rotate-file
+const winston = require('winston')
+require('winston-daily-rotate-file');
+const path = require('path');
+let transports  = [];
+const { createLogger } = winston;
+transports.push(
+    new winston.transports.DailyRotateFile({
+      name: 'file',
+      datePattern: 'YYYY-MM-DD-THH-mm',
+      filename: path.join(__dirname, 'rotate_logs', 'log_file.log')
+    })
+)
+
+const dailyLogger = winston.createLogger({
+    transports: [
+        new winston.transports.DailyRotateFile ({
+            filename: 'application-%DATE%.log',
+            level: 'info',
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d'        
+        })
+    ],
+    exitOnError: false
+});
+
+dailyLogger.stream = {
+    write: function(message, encoding) {
+      logger.info(message);
+    },
+};
+
+export default logger;
+*/
